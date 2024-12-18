e80c852ac552c33a1ce9b53d762d306aba9d7757
Brian Wakhale <brianweke1998@gmail.com>
2024-12-18 07:09:12 +0300

    Created a button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,22 +1,3 @@
-// import React from 'react'
-// import Card from "@/components/Card"
-
-// const Landing: React.FC = () => {
-//   return (
-//     <div>
-//           <h1 className="text-xl font-extralight">Landing Page</h1> 
-//           <Card />
-//           {/* Duplicated Card component */}
-//           <Card />
-//           <Card />
-//           <Card />
-//     </div>
-//   )
-// }
-
-// export default Landing
-
-
 // components/Button.tsx
 import React from 'react';
 
